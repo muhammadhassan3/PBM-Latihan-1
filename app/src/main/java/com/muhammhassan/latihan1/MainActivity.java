f100834d8c376730817dc156c46a73fa2102bfa9
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2024-03-27 22:45:17 +0700

    Finalize

--- app/src/main/java/com/muhammhassan/latihan1/MainActivity.java
+++ app/src/main/java/com/muhammhassan/latihan1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.muhammhassan.latihan1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.muhammhassan.latihan1.databinding.ActivityMainBinding;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -38,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
 
             double nilaiAkhir = getNilaiAkhir(Double.parseDouble(UTS), Double.parseDouble(UAS), Double.parseDouble(lainLain));
 
+            Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra(ResultActivity.EXTRA_NAMA, nama);
+            intent.putExtra(ResultActivity.EXTRA_NIM, NIM);
+            intent.putExtra(ResultActivity.EXTRA_PANGGILAN, panggilan);
+            intent.putExtra(ResultActivity.EXTRA_KELAS, kelas);
+            intent.putExtra(ResultActivity.EXTRA_MATA_KULIAH, mataKuliah);
+            intent.putExtra(ResultActivity.EXTRA_NILAI_AKHIR, nilaiAkhir);
+            intent.putExtra(ResultActivity.EXTRA_JENIS_KELAMIN, getJenisKelamin());
+
+            startActivity(intent);
         });
     }
 
@@ -45,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
         if(binding.radioGroup.getCheckedRadioButtonId() == binding.rbLakiLaki.getId()){
             return "Mas";
         }else return "Mba";
+    }
+    String getJenisKelamin(){
+        if(binding.radioGroup.getCheckedRadioButtonId() == binding.rbLakiLaki.getId()){
+            return "Laki Laki";
+        }else return "Perempuan";
     }
 
     double getNilaiAkhir(double uts, double uas, double lainLain){
--- app/src/main/java/com/muhammhassan/latihan1/ResultActivity.java
+++ app/src/main/java/com/muhammhassan/latihan1/ResultActivity.java
@@ -8,17 +8,66 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.muhammhassan.latihan1.databinding.ActivityResultBinding;
+
 public class ResultActivity extends AppCompatActivity {
+    private ActivityResultBinding binding;
+
+    public static String EXTRA_NAMA = "ext_nama";
+    public static String EXTRA_NIM = "ext_nim";
+    public static String EXTRA_PANGGILAN = "ext_panggilan";
+    public static String EXTRA_JENIS_KELAMIN = "ext_jenis_kelamin";
+    public static String EXTRA_MATA_KULIAH = "ext_mata_kuliah";
+    public static String EXTRA_NILAI_AKHIR = "ext_nilai_akhir";
+    public static String EXTRA_KELAS = "ext_kelas";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_result);
+        binding = ActivityResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String nama = getIntent().getStringExtra(EXTRA_NAMA);
+        String NIM = getIntent().getStringExtra(EXTRA_NIM);
+        String panggilan = getIntent().getStringExtra(EXTRA_PANGGILAN);
+        String jenisKelamin = getIntent().getStringExtra(EXTRA_JENIS_KELAMIN);
+        String mataKuliah = getIntent().getStringExtra(EXTRA_MATA_KULIAH);
+        Double nilaiAkhir = getIntent().getDoubleExtra(EXTRA_NILAI_AKHIR, 0.0);
+        String kelas = getIntent().getStringExtra(EXTRA_KELAS);
+
+        binding.tvNama.setText("Nama Mahasiswa : "+panggilan+" "+nama);
+        binding.tvNIM.setText("NIM : "+NIM);
+        binding.tvJenisKelamin.setText("Jenis Kelamin : "+jenisKelamin);
+        binding.tvMataKuliah.setText("Mata Kuliah : "+mataKuliah);
+        binding.tvNilaiAkhir.setText("Nilai Akhir : "+nilaiAkhir);
+        binding.tvKelas.setText("Kelas : "+kelas);
+        binding.tvGrade.setText("Grade : "+getGrade(nilaiAkhir));
+    }
+
+    String getGrade(double nilaiAkhir){
+        String grade = "Grade Tidak valid";
+        if(nilaiAkhir >80 && nilaiAkhir <= 100){
+            grade = "A";
+        }else if ( nilaiAkhir >=70 && nilaiAkhir <= 80){
+            grade = "AB";
+        } else if (nilaiAkhir >=60 && nilaiAkhir <  80) {
+            grade = "B";
+        } else if (nilaiAkhir >=50 && nilaiAkhir < 60){
+            grade = "BC";
+        } else if (nilaiAkhir >=40 && nilaiAkhir < 50) {
+            grade = "C";
+        }else if (nilaiAkhir >= 30 && nilaiAkhir < 40){
+            grade = "D";
+        }else if(nilaiAkhir >=0 && nilaiAkhir < 30){
+            grade = "E";
+        }
+
+        return grade;
     }
 }
