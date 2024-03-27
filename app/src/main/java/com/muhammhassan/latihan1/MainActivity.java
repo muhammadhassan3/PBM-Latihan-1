961ad29728bada3988289ddd5718bcbdb8b41b89
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2024-03-27 22:07:08 +0700

    Add Logic

--- app/src/main/java/com/muhammhassan/latihan1/MainActivity.java
+++ app/src/main/java/com/muhammhassan/latihan1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.muhammhassan.latihan1;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        binding.btnProses.setOnClickListener(v -> {
+            String panggilan = getPanggilan();
+            String nama = binding.edtNama.getText().toString().trim();
+            String NIM = binding.edtNIM.getText().toString().trim();
+            String kelas = binding.edtKelas.getText().toString().trim();
+            String mataKuliah = binding.edtMatkul.getText().toString().trim();
+            String UTS = binding.edtUTS.getText().toString().trim();
+            String UAS = binding.edtUAS.getText().toString().trim();
+            String lainLain = binding.edtLainLain.getText().toString().trim();
 
+            double nilaiAkhir = getNilaiAkhir(Double.parseDouble(UTS), Double.parseDouble(UAS), Double.parseDouble(lainLain));
+
+        });
+    }
+
+    String getPanggilan(){
+        if(binding.radioGroup.getCheckedRadioButtonId() == binding.rbLakiLaki.getId()){
+            return "Mas";
+        }else return "Mba";
+    }
+
+    double getNilaiAkhir(double uts, double uas, double lainLain){
+        return (uts * 0.3) + (uas * 0.4) + (lainLain * 0.3);
     }
 }
